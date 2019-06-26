5c31fc05379e908e4f74516f95fc657aa2694bcb
Alix Mougenot <alix.mougenot@gmail.com>
2019-06-26 14:46:20 +0200

    Initial Commit

---
+++ ImageCache/ImageCache.h
@@ -0,0 +1,19 @@
+//
+//  ImageCache.h
+//  ImageCache
+//
+//  Created by Alix on 26/06/2019.
+//  Copyright © 2019 AM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCache.
+FOUNDATION_EXPORT double ImageCacheVersionNumber;
+
+//! Project version string for ImageCache.
+FOUNDATION_EXPORT const unsigned char ImageCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCache/PublicHeader.h>
+
+
